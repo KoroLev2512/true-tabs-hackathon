264b7a376755d70bc9ba942a7f3e43f6f1c7a12a
Yurii Korolev <korolev-2512@mail.ru>
2025-04-20 02:45:05 +0300

    moved `message-area` to features

--- widget/chat/chat-title.tsx
+++ widget/chat/chat-title.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import type { ComponentProps, PropsWithChildren } from "react";
+import type { ComponentProps } from "react";
 import { cn } from "@/shared/utils";
+import { useChat } from "@/entities/chat";
 
 export type ChatTitleProps = ComponentProps<"h2">;
 
-export const ChatTitle = ({ className, children, ...props }: PropsWithChildren<ChatTitleProps>) => {
+export const ChatTitle = ({ className, ...props }: ChatTitleProps) => {
+  const title = useChat(store => store.name);
   return (
     <h2 className={cn("font-semibold text-2xl py-4 text-[#F8F8F8]", className)} {...props}>
-      {children}
+      {title}
     </h2>
   );
 };
